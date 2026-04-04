83a4c74d71a2daad12db4e10b078dba6f2f25359
Bonzokoles <bonzokoles@users.noreply.github.com>
2026-04-04 21:48:10 +0200

    cleanup: remove off-plan changes (copilotkit stub, CloudAgentPanel, storage routing)

--- src/ElectronApp.tsx
+++ src/ElectronApp.tsx
@@ -1,10 +1,5 @@
-import { CopilotKit } from '@copilotkit/react-core';
 import { BrowserUI } from './components/browser-core/BrowserUI';
 
 export function ElectronApp() {
-  return (
-    <CopilotKit runtimeUrl="http://127.0.0.1:4111/api/copilotkit" showDevConsole={false} enableInspector={false}>
-      <BrowserUI />
-    </CopilotKit>
-  );
+  return <BrowserUI />;
 }
